581eae8d2f75972a8fd0b39c0233ad8f6df4809c
Snomaskin <mark_0094@icloud.com>
2025-04-09 18:51:57 +0200

    refactor: SearchToggle separated from Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { NavItem } from "./config/navbar";
-import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { Cart } from "../Shop/Cart/Cart";
 import { useSearch } from "../Search/SearchContext";
@@ -8,6 +7,7 @@ import { SearchInput } from "../Search/SearchInput";
 import searchIcon from "../../assets/search.png"
 import closeIcon from "../../assets/close.png"
 import classNames from "classnames";
+import "./Navbar.css";
 
 
 interface NavbarProps {
@@ -15,70 +15,78 @@ interface NavbarProps {
 }
 
 export const Navbar = ({ navItems }: NavbarProps) => {
-    const [ShowHoveredMenu, setShowHoveredMenu] = useState<string | null>(null);
-    const { isSearching, setIsSearching } = useSearch()
+  const [hoveredMenuId, setHoveredMenuId] = useState<string | null>(null);
+  const { isSearching, setIsSearching } = useSearch()
 
-    const handleNavClick = () => {
-        setIsSearching(false);
-    };
+  const handleNavClick = () => {
+      setIsSearching(false);
+  };
 
-    return (
-        <nav className="navbar">
-            {navItems.map((items) => (
-                <div 
-                    className="nav-container" 
-                    key={items.menuId}
-                    onMouseEnter={() => setShowHoveredMenu(items.menuId)}
-                    onMouseLeave={() => setShowHoveredMenu(null)}
-                >
-                    <Link className="nav" 
-                        to={`/${items.menuId}`} 
-                        onClick={handleNavClick}>
-                            {items.menuName}
-                    </Link>
-                    {ShowHoveredMenu === items.menuId && <HoveredMenu items={items} onItemClick={handleNavClick} />}
-                </div>
-            ))}
-            <div className={classNames('nav-container search-container', {
-                'searching': isSearching
-            })}>
-                <div className="nav" onClick={() => setIsSearching((prev) => !prev)}>
-                    <img className="search-icon" src={!isSearching ? searchIcon : closeIcon}/>
-                </div>
-                {isSearching && <SearchInput />}
-            </div>
-            <div className="navbar-right">
-                <Cart />
-            </div>
-        </nav>  
-    );
+  return (
+    <nav className="navbar">
+      {navItems.map((items) => (
+        <div 
+          className="nav-container" 
+          key={items.menuId}
+          onMouseEnter={() => setHoveredMenuId(items.menuId)}
+          onMouseLeave={() => setHoveredMenuId(null)}
+        >
+          <Link className="nav" 
+            to={`/${items.menuId}`} 
+            onClick={handleNavClick}>
+              {items.menuName}
+          </Link>
+          {hoveredMenuId === items.menuId && <HoveredMenu items={items} onItemClick={handleNavClick} />}
+        </div>
+      ))}
+      <SearchToggle isSearching={isSearching} setIsSearching={setIsSearching} />
+      <div className="navbar-right">
+        <Cart />
+      </div>
+    </nav>  
+  );
 };
 
-const HoveredMenu = ({ items, onItemClick }: { items: NavItem; onItemClick: () => void; }) => {
+const HoveredMenu = (
+  { items, onItemClick }: 
+  { items: NavItem; onItemClick: () => void; }) => {
     const [activeItem, setActiveItem] = useState<string | null>(null);
 
     const handleClick = (id: string) => {
-        setActiveItem(id);
-        onItemClick();
+      setActiveItem(id);
+      onItemClick();
     };
 
     return (
-        <div className="dropdown">
-            {items.menuItems?.map((item) => (
-                <Link
-                    className={classNames('list-item', {
-                        'pop-out': activeItem === item.id
-                    })} 
-                    key={item.id} 
-                    to={`${item.path}/${item.id}`}
-                    onClick={() => handleClick(item.id)}
-                >
-                    <img 
-                        className={items.menuId === "shop" ? "shop-icon" : "app-icon"} 
-                        src={item.img} 
-                    />
-                </Link>
-            ))}
-        </div>
+      <div className="dropdown">
+        {items.menuItems?.map((item) => (
+          <Link
+            className={classNames('list-item', {
+              'pop-out': activeItem === item.id
+            })} 
+            key={item.id} 
+            to={`${item.path}/${item.id}`}
+            onClick={() => handleClick(item.id)}
+          >
+            <img 
+              className={items.menuId === "shop" ? "shop-icon" : "app-icon"} 
+              src={item.img} 
+              />
+          </Link>
+        ))}
+      </div>
     );
 };
+
+const SearchToggle = (
+  { isSearching, setIsSearching }: 
+  { isSearching: boolean, setIsSearching: React.Dispatch<React.SetStateAction<boolean>> }) => (
+    <div className={classNames('nav-container search-container', {
+      'searching': isSearching
+    })}>
+      <div className="nav" onClick={() => setIsSearching((prev) => !prev)}>
+        <img className="search-icon" src={!isSearching ? searchIcon : closeIcon}/>
+      </div>
+      {isSearching && <SearchInput />}
+    </div>
+)
