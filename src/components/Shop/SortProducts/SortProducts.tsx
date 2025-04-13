7bb7ac4dbaa12a5c0fda868d3793dcb4ade645ee
Snomaskin <mark_0094@icloud.com>
2025-04-13 19:17:55 +0200

    feat: responsive sort column. auto collapse on window resize

--- src/components/Shop/SortProducts/SortProducts.tsx
+++ src/components/Shop/SortProducts/SortProducts.tsx
@@ -1,12 +1,32 @@
 import { useShop } from "../ShopContext";
 import type { SortField } from "../ShopContext";
 import type { Options } from "../ShopContext";
-import filterArrows from "../../../assets/filter-arrows.png";
+import sortArrows from "../../../assets/sort-arrows.png";
+import { useState, useEffect } from "react";
+import classNames from "classnames";
 import "./SortProducts.css"
 
 
 export const SortProducts = ({ title }: { title?: string }) => {
   const { sortProducts, options } = useShop();
+  const [collapsed, setCollapsed] = useState<boolean>(window.innerWidth <= 450);
+
+  // useEffect because otherwise 'collapsed' remains 'true' when window size increases.
+  useEffect(() => {
+    const handleWindowResize = () => {
+      if (window.innerWidth > 450) {
+        setCollapsed(false)
+      } else {
+        setCollapsed(true)
+      }
+    };
+
+    window.addEventListener('resize', handleWindowResize);
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize);
+    };
+  }, []);
 
   const getSelectedOption = (): SortField | undefined => {
     if (options.price) return 'price';
@@ -20,39 +40,59 @@ export const SortProducts = ({ title }: { title?: string }) => {
     sortProducts(key);
   };
 
-  const filterOptions = [
+  const toggleCollapse = () => {
+    setCollapsed(!collapsed);
+  }
+
+  const sortOptions = [
     { key: 'price', label: 'Price' },
     { key: 'name', label: 'Name' }
   ];
 
   return (
-    <div className="filter-container">
-      <h2>{title}</h2>
-      <h3>Sort by:</h3>
-      {filterOptions.map(item => {
-        const key = item.key as SortField;
-        
-        return (
-          <div className="filter-option" key={key}>
-            <label htmlFor={key}>{item.label}</label>
-            <input 
-              type="checkbox"
-              id={key}
-              checked={options[key]}
-              onChange={() => handleCheckboxClick(key)}
-              disabled={selectedOption !== undefined && selectedOption !== key}
-            />
-            <button 
-              onClick={() => sortProducts(`${key}_asc` as keyof Options, true)}
-              disabled={!options[key]}
-            >
-              <img 
-                src={filterArrows} 
+    <>
+      <div className={classNames('sort-container', {
+              'collapsed': collapsed
+            })}>
+        <div className="title-row">
+          <h2>{title}</h2>
+          <button 
+            className="toggle-sort" 
+            onClick={toggleCollapse}
+            aria-label={collapsed ? "Expand" : "Collapse"}
+          >
+            {collapsed ? "☰" : "<"}
+          </button>
+        </div>
+        <h3>Sort by:</h3>
+        {sortOptions.map(item => {
+          const key = item.key as SortField;
+          
+          return ( 
+            <div 
+            className="sort-option"
+            key={key}>
+              <label htmlFor={key}>{item.label}</label>
+              <input 
+                type="checkbox"
+                id={key}
+                checked={options[key]}
+                onChange={() => handleCheckboxClick(key)}
+                disabled={selectedOption !== undefined && selectedOption !== key}
               />
-            </button>
-          </div>
-        );
-      })}
-    </div>
+              <button 
+                onClick={() => sortProducts(`${key}_asc` as keyof Options, true)}
+                disabled={!options[key]}
+              >
+                <img 
+                  src={sortArrows} 
+                />
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
+
