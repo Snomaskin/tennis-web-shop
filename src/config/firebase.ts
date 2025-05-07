c817d7eeabc494cb72dff334f2b019cecf2432b6
Snomaskin <mark_0094@icloud.com>
2025-05-07 21:49:49 +0200

    lifted firebase config to the root config folder

---
+++ src/config/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+import { getFirestore, connectFirestoreEmulator } from "firebase/firestore";
+
+
+const firebaseConfig = {
+  projectId: "test",
+  apiKey: "fake-api-key",
+  authDomain: "tennis-web-shop.firebaseapp.com"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+if (location.hostname === "localhost") {
+  connectAuthEmulator(auth, "http://localhost:9099");
+};
+
+const firestore = getFirestore(app);
+if (location.hostname === "localhost") {
+  connectFirestoreEmulator(firestore, "localhost", 8080);
+}
+
+export { auth, firestore };
